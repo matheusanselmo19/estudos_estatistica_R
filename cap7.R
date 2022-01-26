fedaf4fd920f7fd52c31700b3636ef15789aea1c
seixasxbr <mateus_seixas@hotmail.com.br>
2022-01-26 17:08:16 -0300

    files

---
+++ cap7.R
@@ -0,0 +1,17 @@
+#capitulo 7
+# 7.1
+LSL = 4
+USL = 14
+mean = 10
+sd = 2
+nivel.sigma1 = (USL-mean)/sd
+nivel.sigma2 = (mean-LSL)/sd
+
+# 7.2
+resposta = 308537-6210
+print(resposta)
+
+# helicoptero
+# target = 1.4
+# LIE = 1.3
+# 20 lancamentos de cada prototipo
