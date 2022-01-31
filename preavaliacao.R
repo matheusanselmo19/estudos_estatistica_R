bc17c5595ea56b72557c2ee5096313a1a1b2285b
seixasxbr <mateus_seixas@hotmail.com.br>
2022-01-31 10:22:58 -0300

    end preav

--- preavaliacao.R
+++ preavaliacao.R
@@ -46,6 +46,8 @@ proporção = sum(amostra)/length(amostra)
 turno.a = c(113,122,138,124,139,118,139,122,121,131,129,133,144,124,153)
 turno.b = c(141,152,147,147,161,147,169,146,147,144,157,145,136,144,154)
 t.test(turno.a, turno.b, alternative = "less")
+# a média do turno a é menor que a do turno b. dúvida (menor ou igual)
+
 #9
 tempo.9 = c(140,144,150,145,142,139,134,152,147,146,153,145,141,141,137,149,143,144)
 t.test(tempo.9,mu = 147, alternative = "less")
@@ -63,16 +65,40 @@ empresa1 = c(11,12,10,9,13,9,7,8,8,8,11,7,9,10,9)
 empresa2 = c(8,9,8,6,12,7,9,5,8,13,8,7,6,10,9)
 empresa3 = c(11,12,10,9,13,9,7,8,8,8,11,7,9,10,9)
 empresa4 = c(13,16,11,12,11,10,14,12,13,15,13,12,16,9,17)
+# empresa.frame = data.frame(empresa1,empresa2,empresa3,empresa4)
+
+empresa.frame <- read.csv("empresa_prazo_preav.csv", header = TRUE, sep = ",")
+anova.res = aov(prazo ~ empresa, data = empresa.frame)
+summary(anova.res)
+# se o p-valor é menor que 0.05 as médias diferem. Logo, letra b.
 
 #12
+empresa.frame2 <- read.csv("empresa_prazo_preav2.csv", header = TRUE, sep = ",")
+anova.res2 = aov(prazo ~ empresa, data = empresa.frame2)
+summary(anova.res2)
+# p-valor > 0.05, logo as médias são iguais, letra a.
 
+#13
+# r é o coeficiente de correlação de Pearson. Esse coeficiente varia de -1 a 1. Quanto mais
+# próximo de 1 ou -1 mais forte é a correlação entre duas variáveis.
+# r^2 é o coeficiente de determinação. quanto mais próximo de 1, maior a relação entre as variáveis (duvida)
+# V F F F
 
+#14 
+#letra b ou a?
 
 #15
 y = c(151.30,172.40,175.60,180.10,189.30,195.90,198.73)
 x = c(259.3,267.5,281.6,279.5,293.7,289.9,295.4)
-
-
+relation <- lm(y~x)
+relation
+plot(x,y,col = "blue",
+     abline(lm(y~x)),cex = 1.3,pch = 16)
+summary(relation)
+# y = 1.134*x - 138.286
+# r^2 = 0.8936
+# p-valor = 0.001304
+# letra d
 
 # teste 
 
